e5f84047b8f99370f2fb154926b1290332075abf
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2021-08-20 15:06:08 +0100

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'traversy-media-task-tracker';
-}
+export class AppComponent {}
